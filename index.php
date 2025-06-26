e816673d1bd29dbeb21d51a8d7e87e1f1bcb1353
Gatlat Deng Bum <167771085+GatlatJack@users.noreply.github.com>
2025-06-26 12:07:56 -0400

    what i learned today

---
+++ index.php
@@ -0,0 +1,5 @@
+// just to mention a few things i learned today
+//merge branches
+// collabration pipeline, from how I own a remote repo, add collabrators adn resolve confilicts in branch commits
+//another things also, a collabrator does not have to clone the repo into their pc not unless the owner has not given permission to push into the remote restore_include_path
+// 

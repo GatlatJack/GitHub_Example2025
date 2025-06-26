98d4617e809757509f36dde11ce6e62fc274d179
Gatlat Deng Bum <167771085+GatlatJack@users.noreply.github.com>
2025-06-26 12:14:50 -0400

    solve conflicts

---
+++ resolveconflicts.py
@@ -0,0 +1,6 @@
+#solving conflicts in push requests :
+#choose which one of the changes you need and then solve the problem
+#go head and merge the changes
+#MORE!
+#cloning--- same Lab
+#forking --- no push previlledges
